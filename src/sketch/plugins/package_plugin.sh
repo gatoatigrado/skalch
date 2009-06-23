0f036994a8ee3d6b3903fe6c6665ad7f3d4eb65e
Casey Rodarmor <casey@rodarmor.com>
2009-06-23 16:30:49 -0700

    Unbroke plugin!

--- src/sketch/plugins/package_plugin.sh
+++ src/sketch/plugins/package_plugin.sh
@@ -1,14 +1,18 @@
 #!/bin/zsh
 
-TEMP="working_dir"
+TEMP="temp_dir"
+
 JAR="sketchrewriter.jar"
 SRC="SketchRewriter.scala"
+XML="scalac-plugin.xml"
+
 TEST="PluginTest"
 
 rm $JAR
 mkdir $TEMP
-fsc -d $TEMP $SRC
-cp scalac-plugin.xml $TEMP
+scalac -d $TEMP $SRC
+cp $XML $TEMP/scalac-plugin.xml
 (cd $TEMP; jar cf ../$JAR .)
 rm -rf $TEMP
 scalac -Xplugin:$JAR $TEST.scala && scala $TEST
+rm *.class

0f8f9d00a8f60fbcd496f7da52a0483908ec1b4f
Nicholas Tung <gatoatigrado@gmail.com>
2009-07-13 13:15:43 -0700

    [sketches.gatoatigrado] functional unfaithful test

--- src/test/df/AbstractDfSketch.scala
+++ src/test/df/AbstractDfSketch.scala
@@ -3,7 +3,7 @@ package test.df
 import skalch.DynamicSketch
 
 abstract class AbstractDfSketch(val num_buckets : Int) extends DynamicSketch {
-    case class Pebble(val order : Boolean, val color : String) {
+    case class Pebble(val order : Int, val color : String) {
         override def toString = color
     }
 
@@ -16,7 +16,11 @@ abstract class AbstractDfSketch(val num_buckets : Int) extends DynamicSketch {
     def read_from_input() = {
         var i = 0
         while (i < num_buckets) {
-            buckets(i) = next_int_input()
+            buckets(i) = next_int_input() match {
+                case 0 => Red()
+                case 1 => White()
+                case 2 => Blue()
+            }
             i += 1
         }
     }
@@ -28,15 +32,31 @@ abstract class AbstractDfSketch(val num_buckets : Int) extends DynamicSketch {
             if (buckets(i).order > buckets(i + 1).order) {
                 return false
             }
+            i += 1
         }
         true
     }
 
-    val tests = new TestGenerator() {
+    def swap(i : Int, j : Int) {
+        val tmp = buckets(i)
+        buckets(i) = buckets(j)
+        buckets(j) = tmp
+    }
+
+    final def dysketch_main() : Boolean = {
+        read_from_input()
+        df_main()
+        isCorrect(buckets.length)
+    }
+
+    def df_main()
+
+    val test_generator = new TestGenerator() {
         val arr = Array(White(), Blue(), Red())
+        assert(arr.length == num_buckets)
         def set() {
             for (a <- 0 until num_buckets) {
-                put_default_input(arr(a))
+                put_default_input(arr(a).order)
             }
         }
         def tests() { test_case() }
--- src/test/df/DfUnfaithfulTest.scala
+++ src/test/df/DfUnfaithfulTest.scala
@@ -1,12 +1,22 @@
 package test.df
 
+import sketch.dyn.BackendOptions
+import sketch.util._
+import DebugOut.print
+
 class DfUnfaithfulSketch(num_buckets : Int) extends AbstractDfSketch(num_buckets) {
+    def df_main() {
+        for (i <- (0 to !!(num_buckets))) {
+            import java.lang.Integer
+            swap(!!(buckets.length), !!(buckets.length))
+        }
+    }
 }
 
 object DfUnfaithfulTest {
     object TestOptions extends cli.CliOptionGroup {
         import java.lang.Integer
-        add("--num_buckets", 10 : Integer, "length of list")
+        add("--num_buckets", 10 : Integer, "number of buckets (each with one pebble)")
     }
 
     def main(args : Array[String]) = {
@@ -14,6 +24,6 @@ object DfUnfaithfulTest {
         val opts = TestOptions.parse(cmdopts)
         BackendOptions.add_opts(cmdopts)
         skalch.synthesize(() => new DfUnfaithfulSketch(
-            opts.long_("num_buckets").intValue)
+            opts.long_("num_buckets").intValue))
     }
 }

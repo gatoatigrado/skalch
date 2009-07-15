b501057bfa57c91887a82eac8cec31cdc6c8f170
Casey Rodarmor <casey@rodarmor.com>
2009-07-15 12:34:03 -0700

    Cleaned up some dead code, also removed 'x until y' constructs (they're broken on my machine)

--- src/test/Dfs.scala
+++ src/test/Dfs.scala
@@ -8,7 +8,6 @@ import scala.collection.mutable.Stack
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.mutable.Buffer
 import scala.collection.mutable.ListBuffer
-import scala.runtime.RichInt
 
 class DfsSketch() extends DynamicSketch {
     def dysketch_main() = {
@@ -26,12 +25,6 @@ class DfsSketch() extends DynamicSketch {
         true
     }
 
-    /*
-    each child with multiple children gives the oracle to re-order those children, and the fix them sometime on the way up, giving more valid traces
-
-    a node with two children that point to the same node will also introduce new multiple traces
-    */
-
     class Graph(n: Int, seed: Long = 1983) {
         val nodes = new ArrayBuffer[Node]
 
@@ -44,44 +37,10 @@ class DfsSketch() extends DynamicSketch {
         e.children += a
         d.children += b
 
-/*
-        val e = new Node("e")
-        val d = new Node("d", e)
-        val c = new Node("c", d, e)
-        val b = new Node("b", c, e)
-        val a = new Node("a", b)
-*/    
         val root = a
 
         nodes ++= List(a,b,c,d,e)
         
-        /* // code for randomly generating a graph
-        {
-            var i: Int = 0
-
-            import scala.util.Random
-          
-            i = 0
-            while(i < n) {
-                nodes += new Node("" + i)
-                i += 1
-            }
-
-            val rng = new Random
-            rng.setSeed(seed)
-
-            i = 0
-            while(i < n * 10) {
-                val parent = nodes(rng.nextInt(nodes.length))
-                val child  = nodes(rng.nextInt(nodes.length))
-                parent.children += child
-                i += 1
-            }
-
-            nodes(0)
-        }
-        */
-
         def checkpoint() {
             for(node <- nodes) {
                 node.checkpoint()
@@ -206,16 +165,17 @@ class DfsSketch() extends DynamicSketch {
 
         val extraStorage = new ArrayBuffer[A]
 
-        for (i <- 1 to allowedExtraStorage) {
+        var i = 0
+        while(i < allowedExtraStorage) {
             extraStorage += !!(domainA)
+            i += 1
         }
         
-        val popp1 = !!(0 until 3)
-        val popp2 = !!(0 until 2)
+        val popp1 = !!(List(0, 1, 2))
+        val popp2 = !!(List(0, 1))
 
-        val pushp1 = !!(0 until 3)
-        val pushp2 = !!(0 until 2)
-//        val pushp3 = !!(0 until 1)
+        val pushp1 = !!(List(0, 1, 2))
+        val pushp2 = !!(List(0, 1))
         
         val extraLocations = mkLocations(extraStorage)
 
@@ -240,31 +200,11 @@ class DfsSketch() extends DynamicSketch {
             for(location <- extraLocations) {
                 values += location.read
             }
-            // for (i <- 1 to values.length) {
-            //    // generate the last index first
-            //    permutation.push(!!(i))
-            // }
-            //for(location <- allLocations) {
-            //      location.write(values.remove(!!(values.length)))
-                  // location.wite(values.remove(permutation.pop))
-            //}
             
-            borrowedLoc.write(values.remove(pushp1))//!!(values.length))) 
-            extraLocations(0).write(values.remove(pushp2))//!!(values.length))) 
-            // extraLocations(1).write(values.remove(!!(values.length))) 
-
-            //- borrowedLoc.write(x)  // mistake!
-            //- extraLocations(0).write(borrowedVal)
+            borrowedLoc.write(values.remove(pushp1))
+            extraLocations(0).write(values.remove(pushp2))
             
-            // an attempt to determinize (quite successful on a chain graph)
-            // store the value from borrowed location into the local location
-            //- synthAssertTerminal(borrowedVal == extraLocations(0).read)
-            // traces that remain have same effect because x and extraLoc have same value
-            // so this assert had no effect
-            //    synthAssertTerminal(x == borrowedLoc.read)
-            // we could fix it by setting the permutation just so; see above
-
-            synthAssertTerminal(x == extraLocations(0).read)  // enocurage? to keep a value for 1 or 2 operations
+            synthAssertTerminal(x == extraLocations(0).read)
             
             skdprint(extraStorage.mkString("push("+x.toString()+") : (", ", ", ")") + " borrowedVal:[" + borrowedVal.toString() + "]")
         }
@@ -282,8 +222,6 @@ class DfsSketch() extends DynamicSketch {
             val borrowedLoc = !!(potentialBorrowedLocations)
             synthAssertTerminal(borrowedLoc == refBorrowedLoc)
 
-            // val borrowedVal = refBorrowedLoc.read
-
             var allLocations = List(refBorrowedLoc) ++ extraLocations
             val values = new ListBuffer[A]
             var found = false
@@ -298,21 +236,11 @@ class DfsSketch() extends DynamicSketch {
 
             skdprint(extraStorage.mkString("pop: (", ", ", ")") + " borrowdVal:[" + refBorrowedLoc.read.toString() + "] returns:" + refPoppedVal.toString())
 
-            // this shuffle of values among allLocations will also restore the value
-     
-            //for(location <- allLocations) {
-            //    location.write(values.remove(!!(values.length)))
-            //}
-            
-            refBorrowedLoc.write(values.remove(popp1))//!!(values.length))) 
-            extraLocations(0).write(values.remove(popp2))//!!(values.length))) 
-            // extraLocations(1).write(values.remove(!!(values.length)))    one extra need not be restored
+            refBorrowedLoc.write(values.remove(popp1))
+            extraLocations(0).write(values.remove(popp2))
             
             skdprint(extraStorage.mkString("pop: (", ", ", ")") + " borrowdVal:[" + refBorrowedLoc.read.toString() + "] returns:" + refPoppedVal.toString())
-            // determinization: attempt to go from 2 traces to 1
-            // successful: this looks like a good invariant to infer!
-            // at the end of the execution, it does no tmatter how we restore, hence we had two traces
-            //- synthAssertTerminal(refPoppedVal == extraLocations(0).read)
+
             refPoppedVal
         }
     }
@@ -359,7 +287,6 @@ class DfsSketch() extends DynamicSketch {
             }
 
             if(next != null) {
-                // last two arguments give node whose children fields are available to the stack as borrowed locations
                 stack.push(current, mkLocations(current.children))
                 skdprint("graph after push:" + g.toString())
                 current = next

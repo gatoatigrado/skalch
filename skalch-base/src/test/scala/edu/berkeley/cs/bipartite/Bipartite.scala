c17bef137d7c8498533bd9ca5457d17b836a28bd
Shaon Barman <sbarman@eecs.berkeley.edu>
2010-07-09 18:20:57 -0400

    Modified bipartite

--- skalch-base/src/test/scala/edu/berkeley/cs/bipartite/Bipartite.scala
+++ skalch-base/src/test/scala/edu/berkeley/cs/bipartite/Bipartite.scala
@@ -59,7 +59,7 @@ class BipartiteSketch extends AngelicSketch {
           var edges : Array[Edge] = new Array[Edge](MAXE)
 
           
-          def checkBipartite() = {
+          def checkBipartiteV0() = {
                for (j <- 0 to ne - 1) { // repeat #edges times
                      val e : Edge = edges(!!(ne))
                      synthAssert (! e.handled)
@@ -71,6 +71,54 @@ class BipartiteSketch extends AngelicSketch {
                      synthAssert (beta())
                }
           }
+          
+          // wavefront
+          def checkBipartiteV1(root : Vertex) = {
+               for (j <- 0 to ne - 1) { // repeat #edges times
+                     val e : Edge = edges(!!(ne))
+                     synthAssert (! e.handled)
+                     e.handled = true
+                     if (j == 0) { 
+                         synthAssert (e.src == root)
+                         e.src.inset = 0
+                     } else {
+                         synthAssert ( e.src.inset != -1)  
+                         // src of this edge is a vertex whose polarity has been fixed already
+                     }
+                     skdprint("Selecting Edge " + e)
+                     e.dest.inset = 1 - e.src.inset                                   
+                     synthAssert (beta())
+               }
+          }
+                
+          // wavefront, polarity angels, flip computation
+          def checkBipartiteV2(root : Vertex) = {
+               var curp : Int = 1
+               var flipcnt : Int = 0;
+               for (j <- 0 to ne - 1) { // repeat #edges times
+                     val e : Edge = edges(!!(ne))
+                     synthAssert (! e.handled)
+                     e.handled = true
+                     if (j == 0) { 
+                         synthAssert (e.src == root)
+                         e.src.inset = 0
+                     } else {
+                         synthAssert ( e.src.inset != -1)  
+                         // src of this edge is a vertex whose polarity has been fixed already
+                     }
+                     skdprint("Selecting Edge " + e)
+                     e.dest.inset = !!(0,1) // 1 - e.src.inset
+                     if (e.dest.inset != curp) {
+                         curp = e.dest.inset
+                         flipcnt = flipcnt + 1
+                         // synthAssert (flipcnt < 4) // don't flip too often
+                     }
+                     synthAssert (beta())
+               }
+               skdprint("flips " + flipcnt)
+          }
+          
+          // at this time i am seeing a lot of 
           
 	      def beta() : Boolean = {
 	      	  for (j <- 0 to ne - 1) {
@@ -109,27 +157,57 @@ class BipartiteSketch extends AngelicSketch {
               }
         }
 
-	var vA : Vertex = new Vertex("a")
-	var vB : Vertex = new Vertex("b")
-	var vC : Vertex = new Vertex("c")
-	var vD : Vertex = new Vertex("d")
+	//var vA : Vertex = new Vertex("a")
+	//var vB : Vertex = new Vertex("b")
+	//var vC : Vertex = new Vertex("c")
+	//var vD : Vertex = new Vertex("d")
 	
-	var G : Graph = new Graph()
+	//var G : Graph = new Graph()
 	
-	G.addVertex(vA)
-	G.addVertex(vB)
-	G.addVertex(vC)
-	G.addVertex(vD)
+	//G.addVertex(vA)
+	//G.addVertex(vB)
+	//G.addVertex(vC)
+	//G.addVertex(vD)
 		
-	G.addEdge(vC,vD)
-	G.addEdge(vA,vB)
-    G.addEdge(vA,vC)	
-	G.addEdge(vB,vD)
-
+	//G.addEdge(vC,vD)
+	//G.addEdge(vA,vB)
+    //G.addEdge(vA,vC)	
+	//G.addEdge(vB,vD)
+        
+    var G : Graph = new Graph()
+    var v0 : Vertex = new Vertex("0")
+    var v1 : Vertex = new Vertex("1")
+    var v2 : Vertex = new Vertex("2")
+    var v3 : Vertex = new Vertex("3")
+    var v4 : Vertex = new Vertex("4")
+    var v5 : Vertex = new Vertex("5")
+    var v6 : Vertex = new Vertex("6")
+    var v7 : Vertex = new Vertex("7")
+    
+    G.addVertex(v0)
+    G.addVertex(v1)
+    G.addVertex(v2)
+    G.addVertex(v3)
+    G.addVertex(v4)
+    G.addVertex(v5)
+    G.addVertex(v6)
+    G.addVertex(v7)
+    
+    G.addEdge(v6,v4)
+    G.addEdge(v2,v0)
+    G.addEdge(v0,v1)
+    G.addEdge(v7,v6)
+    G.addEdge(v0,v5)
+    G.addEdge(v0,v7)
+    G.addEdge(v1,v3)
+    G.addEdge(v5,v6)
+    G.addEdge(v6,v2)
+    G.addEdge(v4,v0)
+    
 	// so far, the angels in S are able to do the right thing, no matter in which
 	// order we feed the edges. to think about entanglement.
 	
-	G.checkBipartite()
+	G.checkBipartiteV2(v0)
 	G.printBipartite()
      }
 }
